15de139e6665c9b653393c57917c2cdfe7b94115
JOUIDIR <w.jouidir@gmail.com>
2022-05-06 17:22:03 +0200

    push for issue

---
+++ src/Controller/Api/EventController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\Event;
+use App\Entity\Association;
+use App\Models\CustomJsonError;
+use App\Repository\EventRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+
+
+/**
+ * 
+ * @Route("/api/events", name="api_events_")
+ * 
+ */
+class EventController extends JsonController
+{
+    /**
+     * @Route("", name="browse", methods={"GET})
+     * 
+     */
+    public function index(EventRepository $repo): Response
+    {
+        $allEvents = $repo->findAll();
+
+        return $this->json200($allEvents,
+            ["api_event"]
+    );
+    }
+}
