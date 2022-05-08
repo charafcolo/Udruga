e1aabfbc551a89e7a0c8ecbe7b780175f39c60fc
charafcolo <charafcolo@hotmail.fr>
2022-05-09 00:34:21 +0200

    Methods findById, create and delete for User entity

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -19,11 +19,12 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 /**
  * 
  * @Route("/api/users", name="api_users_")
- * 
+ * @Route("", name="browse", methods={"GET"}, requirements={"id":"\d+"})
  */
 class UserController extends JsonController
 {
     /**
+     * List users
      * @Route("", name="browse", methods={"GET"})
      * 
      */
@@ -35,5 +36,80 @@ class UserController extends JsonController
         ["api_user"]
     
     );
+    }
+
+        /**
+     * Read user by ID
+     * 
+     * @Route("/{id}", name="read", methods={"GET"}, requirements={"id":"\d+"})
+     *
+     * 
+     */
+    public function user(User $user = null): Response 
+    {
+        /* si le parm converter n'a rien avec l'id $event est null */
+        if ($user === null) {
+            //on renvoi du JSON 404
+            return $this->json404("il n'existe pas d'user avec cet ID");
+        }
+        return $this->json200($user, ["api_user"]);
+    }
+
+    /**
+     * Create user
+     *
+     * @Route("", name="add", methods={"POST"})
+     * 
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $em
+     * @return JsonResponse
+     */
+    public function create(
+        Request $request,
+        SerializerInterface $serializer,
+        EntityManagerInterface $em 
+        ): JsonResponse
+    {
+        // Récupérer le contenu JSON
+        $jsonContent = $request->getContent();
+
+        // Désérialiser (convertir) le JSON en entité Doctrine User
+        $user = $serializer->deserialize($jsonContent, User::class, 'json');
+         
+        // On sauvegarde l'entité
+        $em->persist($user);
+        $em->flush();
+
+        // TODO : return 201
+        return $this->json(
+            $user,
+            // je précise que tout est OK de mon coté en précisant que la création c'est bien passé
+            // 201
+            Response::HTTP_CREATED,
+            [],
+            [
+                "groups" => 
+                [
+                    "api_user"
+                ]
+            ]
+        );
+    }
+
+    /**
+     * Delete user
+     * @Route("/{id}/delete", name="delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, User $user, UserRepository $userRepository, EntityManagerInterface $em): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $userRepository->remove($user);
+        }
+
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json200($user, ["api_user"]);
     }
 }
