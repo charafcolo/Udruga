9059930d7b4c47640c6e4adb5d8b20d40b4743bf
JOUIDIR <w.jouidir@gmail.com>
2022-05-13 12:44:40 +0200

    Add .htaccess and change some security.yaml path

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -12,24 +12,25 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 class ApiLoginController extends AbstractController
 {
     /**
-     * @Route("/api/login", name="api_login")
+     * @Route("/api/login", name="api_login", methods={"POST"})
      */
-    public function index(): Response
+    public function index(User $user, TokenInterface $token): Response
     {
   
-        // if (null === $user) {
-        //                return $this->json([
-        //                    'message' => 'missing credentials',
-        //                ], Response::HTTP_UNAUTHORIZED);
-        //            }
+         if (null === $user) {
+                        return $this->json([
+                            'message' => 'missing credentials',
+                        ], Response::HTTP_UNAUTHORIZED);
+                    }
         
-    //    $token = $token; // somehow create an API token for $user
+        $token = $token; // somehow create an API token for $user
 
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ApiLoginController.php',
-            // 'user'  => $user->getUserIdentifier(),
-            // 'token' => $token,
+            /* 'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ApiLoginController.php', */
+
+             'user'  => $user->getUserIdentifier(),
+             'token' => $token,
         ]);
     }
 }
