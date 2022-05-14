c00ddfbc14c31630e72d9350e2f1d749a3dc1acc
charafcolo <charafcolo@hotmail.fr>
2022-05-14 23:58:00 +0200

    migration for deploiment 2

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -6,7 +6,6 @@ namespace App\Entity;
 use App\Entity\Association;
 use App\Entity\User;
 use App\Repository\EventRepository;
-use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
